2e4be427bd9de28cdfce1022022eebd7d64fb849
hellokuzuki <miximixi_wang@hotmail.com>
2016-04-12 17:38:43 +1000

    add mouse event test

--- ddf/src/test/java/selniumAPI/MouseAndKeyboardEventTest.java
+++ ddf/src/test/java/selniumAPI/MouseAndKeyboardEventTest.java
@@ -2,14 +2,19 @@ package selniumAPI;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import utility.BrowserFactory;
 
@@ -27,7 +32,7 @@ public class MouseAndKeyboardEventTest {
 	@Before
 	public void setUp() throws Exception {
 
-		driver.get("http://www.davidjones.com.au");
+		driver.get("http://component-showcase.icesoft.org/component-showcase/showcase.iface");
 		System.out.println("### SET UP EACH TEST ###");
 	}
 
@@ -39,24 +44,80 @@ public class MouseAndKeyboardEventTest {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		driver.quit();
+//		driver.quit();
 		System.out.println("### TEAR DOWN AFTER CLASS###");
 	}
 
+	@Ignore
 	@Test
-	public void testElementStatus() {
-		WebElement signup = driver.findElement(By.linkText("Create an Account"));
-		signup.click();
+	public void testMouseAndKeyboard() {
 
-		if (driver.findElement(By.xpath("//input[@name='sendMeEmail']")).isEnabled()) {
-			if (!driver.findElement(By.xpath("//input[@name='sendMeEmail']")).isSelected()) {
-				driver.findElement(By.xpath("//*[@id=\"WC_UserRegistrationAddForm_div_3\"]/fieldset/ul/li[2]/label"))
-						.click();
-			} else {
-				System.out.println("Element is selected !!");
-			}
-		} else {
-			fail("Element is disabled !!");
-		}
+		driver.findElement(By.linkText("Table")).click();
+		
+		driver.findElement(By.linkText("Row Selection")).click();
+		
+		driver.findElement(By.xpath("//label[@class='iceSelOneRb' and text()='Multiple']")).click();
+		
+		List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:employeeList\"]/tbody/tr"));
+		
+		System.out.println(rows.size());
+		
+		Actions builder = new Actions(driver);
+		builder.keyDown(Keys.CONTROL)
+				.click(rows.get(0))
+				.click(rows.get(2))
+				.click(rows.get(5))
+				.keyUp(Keys.CONTROL)
+				.build()
+				.perform();
+		
+		List<WebElement> trs = driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr"));
+		assertEquals(3, trs.size());
+	}
+	
+	@Test
+	public void testMouseAndKeyboardWithShift(){
+
+		driver.findElement(By.linkText("Table")).click();
+		
+		driver.findElement(By.linkText("Row Selection")).click();
+		
+		driver.findElement(By.xpath("//label[@class='iceSelOneRb' and text()='Multiple']")).click();
+		
+		List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:employeeList\"]/tbody/tr"));
+		
+		System.out.println(rows.size());
+		
+		Actions builder = new Actions(driver);
+		builder.click(rows.get(0))
+				.keyDown(Keys.SHIFT)
+				.click(rows.get(2))
+				.click(rows.get(5))
+				.click(rows.get(6))
+				.keyUp(Keys.SHIFT)
+				.build()
+				.perform();
+		
+		WebElement tbody = driver.findElement(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody"));
+		List<WebElement> trs = driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr"));
+		
+//		for (int i = 0; i <500 ; i++) {
+//			driver.findElement(By.xpath("//label[@class='iceSelOneRb' and text()='Multiple']")).click();
+//		}
+//		
+//		for (WebElement tr : trs) {
+//			List<WebElement> tds = driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr/td"));
+//			for (WebElement td : tds) {
+//			}
+//		}
+		
+//		for (int i = 0; i <= trs.size(); i++) {
+			System.out.println(driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr")).get(0).getText());
+//		}
+			System.out.println(driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr")).get(1).getText());
+			System.out.println(driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr")).get(2).getText());
+			System.out.println(driver.findElements(By.xpath("//*[@id=\"iceform:icepnltabset:0:selectedRows\"]/tbody/tr")).get(3).getText());
+
+		assertEquals(4, trs.size());
 	}
 }
