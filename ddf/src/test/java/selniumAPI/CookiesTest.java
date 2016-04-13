3a3a5a76ebdcd3655427d10478d28c39bcbf1396
hellokuzuki <miximixi_wang@hotmail.com>
2016-04-13 22:29:18 +1000

    add cookie test case

---
+++ ddf/src/test/java/selniumAPI/CookiesTest.java
@@ -0,0 +1,56 @@
+package selniumAPI;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import utility.BrowserFactory;
+
+public class CookiesTest {
+
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		driver = BrowserFactory.getBrowser("Chrome");
+		System.out.println("### SET UP BEFORE CLASS ###");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+		driver.get("http://www.davidjones.com.au");
+		System.out.println("### SET UP EACH TEST ###");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+		System.out.println("### TEAR DOWN EACH TEST ###");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		driver.quit();
+		System.out.println("### TEAR DOWN AFTER CLASS###");
+	}
+
+	@Test
+	public void testCookies() {
+		Cookie cookie = driver.manage().getCookieNamed("yieldify_visit");
+		
+		assertEquals("1", cookie.getValue());
+	}
+
+
+}
--- ddf/src/test/java/selniumAPI/ExecutingJavascriptTest.java
+++ ddf/src/test/java/selniumAPI/ExecutingJavascriptTest.java
@@ -8,11 +8,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
